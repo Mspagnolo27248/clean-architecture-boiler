df94077f746dcb0eb0536abee21fa17307853a9a
Michael Spagnolo <mspagnolo27@gmail.com>
2024-10-13 14:21:53 -0400

    added tests

--- src/core-layer/tests/unitconvert.test.ts
+++ src/core-layer/tests/unitconvert.test.ts
@@ -1,5 +1,6 @@
 
 import { UnitOfMeasureConverterService } from "../order-entry-module/domain-services/UnitOfMeasureConverterService";
+import { UnitOfMeasure } from "../order-entry-module/enums/order-entry-enums";
 
 
 const mockUOMAndGallonFactor: Record<string, { unitsOfMeasureInAContainer: number; gallonsInAContainer: number }> = {
@@ -8,17 +9,17 @@ const mockUOMAndGallonFactor: Record<string, { unitsOfMeasureInAContainer: numbe
     "7730|469|EA": { unitsOfMeasureInAContainer: 1, gallonsInAContainer: 4.73 },
 };
 
-// const inputsToConvert = {
-// product: '7946', 
-// apiGravity: 30, 
-// container: '846', 
-// uom: 'EA', 
-// pricePerUnitOfMeasure: 7.77, 
-// qtyOfContainers: 1 
-// }
+const mockInput1 = {
+product: '7946', 
+apiGravity: 30, 
+container: '846', 
+uom: 'EA', 
+pricePerUnitOfMeasure: 7.77, 
+qtyOfContainers: 1 
+}
 
 
-const inputsToConvert = {
+const mockInput2 = {
     product: '7776', 
     apiGravity: 27.4, 
     container: '846', 
@@ -26,5 +27,24 @@ const inputsToConvert = {
     pricePerUnitOfMeasure: 1.468, 
     qtyOfContainers: 1 
     }
-const results = UnitOfMeasureConverterService(inputsToConvert,mockUOMAndGallonFactor);
+
+const mockInput3 = {
+    product: '7776', 
+    apiGravity: 27.4, 
+    container: '022', 
+    uom: 'GAL', 
+    pricePerUnitOfMeasure: 10.14, 
+    qtyOfContainers: 1 
+    }
+
+    const mockInput4 = {
+        product: '7776', 
+        apiGravity: 27.4, 
+        container: '022', 
+        uom: UnitOfMeasure.LI, 
+        pricePerUnitOfMeasure: 1.99, 
+        qtyOfContainers: 1 
+        }
+
+const results = UnitOfMeasureConverterService(mockInput4,mockUOMAndGallonFactor);
 console.log(JSON.stringify(results));
